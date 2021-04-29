40e87956dcf789d9df7c5e4025b9691481e91680
thallesgalv <thallesgalv@gmail.com>
2021-04-29 19:58:19 -0300

    api base

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,11 +1,25 @@
 import Button from '../components/Button'
 
-const Test: React.FC = () => {
+const Test: React.FC = (props) => {
+
   return (
     <div style={{ height: '100vh', display: 'grid', placeItems: 'center' }}>
       <Button>Vamos trabalhar juntos?</Button>
+
+      <p>{JSON.stringify(props.techs.name)}</p>
+
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const response = await fetch ('http://localhost:3000/api/techs')
+  const data = await response.json()
+  return {
+    props: {
+      techs: data
+    }
+  }
 }
 
 export default Test
--- src/template/Portifolio/styles.ts
+++ src/template/Portifolio/styles.ts
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  /* margin-top: 30rem; */
   padding: 30rem 4rem;
 
   > p {

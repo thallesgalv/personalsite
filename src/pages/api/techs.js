cb9644ed0a828f105acc84ecc6ff77830fad63ff
thallesgalv <thallesgalv@gmail.com>
2021-04-29 20:24:25 -0300

    fetching internal api

--- src/pages/api/techs.js
+++ src/pages/api/techs.js
@@ -1,3 +1,10 @@
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export async function getData() {
+  const response = await fetch(/* external API endpoint */)
+  const jsonData = await response.json()
+  return jsonData
+}
+
+export default async function handler(req, res) {
+  const jsonData = await getData()
+  res.status(200).json(jsonData)
 }
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,19 +1,23 @@
 import Button from '../components/Button'
+import handler from '../pages/api/techs'
 
-const Test: React.FC = (props) => {
+const Test: React.FC = props => {
+  console.log(handler())
 
   return (
     <div style={{ height: '100vh', display: 'grid', placeItems: 'center' }}>
       <Button>Vamos trabalhar juntos?</Button>
-
-      <p>{JSON.stringify(props.techs[0].id)}</p>
-
+      {/*
+      <p>{JSON.stringify(props.techs[0].id)}</p> */}
+      <p>{JSON.stringify(props.techs.name)}</p>
     </div>
   )
 }
 
 export async function getServerSideProps() {
-  const response = await fetch ('https://ranekapi.origamid.dev/json/api/produto/')
+  const response = await fetch(
+    'https://personalsite-peach.vercel.app/api/techs'
+  )
   const data = await response.json()
   return {
     props: {

0dfd6b754559ebefac7a6cacbda1bc2675898da3
thallesgalv <thallesgalv@gmail.com>
2021-04-29 20:28:59 -0300

    fixing fetch

--- src/pages/api/techs.js
+++ src/pages/api/techs.js
@@ -1,10 +1,3 @@
-export async function getData() {
-  const response = await fetch(/* external API endpoint */)
-  const jsonData = await response.json()
-  return jsonData
-}
-
-export default async function handler(req, res) {
-  const jsonData = await getData()
-  res.status(200).json(jsonData)
+export default function handler(req, res) {
+  res.status(200).json({ name: 'John Doe' })
 }
